46ea8171006d1545e0feec18bd725296a2674290
Nelan <49295389+NelanJoe@users.noreply.github.com>
2022-12-08 15:59:07 +0700

    Delete use_modul_jadwal_sholat.py

--- use_modul_jadwal_sholat.py
+++
@@ -1,7 +0,0 @@
-from modul_jadwal_sholat import jadwal_sholat
-
-# Definisikan URL / alamat dari sebuah situs
-url = "https://www.muslimpro.com/id/"
-
-# Panggil function jadwal_sholat
-jadwal_sholat(url)
